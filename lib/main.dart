25e9f5d1baa1f477b4b3b98732394172eaaeb1f5
KarinaMoraaa <133887239+KarinaMoraaa@users.noreply.github.com>
2025-11-30 20:31:40 -0600

    Navegadores

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:mediaquery/Scrolls.dart';
+import 'package:navegadores/pantallas/pantalla1.dart';
 
-void main() => runApp(const MyApp());
 
+void main(){
+  runApp(const MyApp());
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final tamanio = MediaQuery.of(context).size;
-    final ancho = tamanio.width;
-    final alto = tamanio.height;
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Navegador con Material 2',
+    
+    theme: ThemeData(
+      brightness: Brightness.light,
+      primarySwatch: Colors.blue,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        elevation: 4,
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold
+        ),
 
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white, 
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20, 
+            vertical: 12  
+            ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          )
+          )
+      )
+    ),
+    home: const Pantalla1(),
+  );
 
-    return MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      home: ClaseScroll(),
-    );
-  }
+}
 }
